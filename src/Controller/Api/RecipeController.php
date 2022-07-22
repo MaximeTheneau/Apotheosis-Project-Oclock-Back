f4b6114bf6bc1f4204a722219e3b66ed11f71314
Adrien <adrienpinilla@gmail.com>
2022-07-22 12:11:25 +0200

    fix bug double requset httpFundation OK

--- src/Controller/Api/RecipeController.php
+++ src/Controller/Api/RecipeController.php
@@ -9,7 +9,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/api/recipes", name="app_api_recipes_")
