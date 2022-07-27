7f6b6b17431a13b1c0b6743712af1de21cb0ac81
NathalieTournon <ntournon@gmail.com>
2022-07-27 08:15:14 +0200

    Backoffice CRUD RecipeIngredient OK

---
+++ src/Form/RecipeIngredientType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\RecipeIngredient;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecipeIngredientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('unit')
+            ->add('quantity')
+            ->add('recipe')
+            ->add('ingredient')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => RecipeIngredient::class,
+        ]);
+    }
+}
