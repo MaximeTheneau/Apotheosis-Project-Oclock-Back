e4bbb16ef8b908b88c6e9d4acaacb15666a46c14
NathalieTournon <ntournon@gmail.com>
2022-08-01 14:19:48 +0200

    Button + connexion + deconnexion backoffice

--- src/Security/OmiamAuthenticator.php
+++ src/Security/OmiamAuthenticator.php
@@ -49,14 +49,14 @@ class OmiamAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        if ($this->security->isGranted("ROLE_USER")) {
-            return new RedirectResponse($this->urlGenerator->generate('error403'));
-        }
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        // if ($this->security->isGranted("ROLE_USER")) {
+        //     return new RedirectResponse($this->urlGenerator->generate('error403'));
+        // }
+        return new RedirectResponse($this->urlGenerator->generate('app_back_home'));
 
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
